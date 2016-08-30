d019f7ea5df6bd314120ae2b9868f64c750fa515
lyangdong <330338945@qq.com>
2016-08-31 00:32:27 +0800

    123

---
+++ public.js
@@ -0,0 +1,30 @@
+/**
+ * Created by liu on 2016/7/9.
+ */
+$(function(){
+        $('.box').mouseenter(function(){
+            var x =  $(this).position().left
+            var y =$(this).find('ul').width();
+            if(x+y >$('.nav').innerWidth()){
+                $(this).find('li').css('float','right')
+                $(this).find('ul').css('right',0)
+            }
+        })
+
+    });
+//for(var i=0;i<$('.nav div').length;i++){
+//    $('.nav div').eq(i).css('left',i*100);
+//    var x =$('.nav div').eq(i).position().left+$('.nav div').eq(i).find('span').innerWidth()+20;
+//    if(x+$('.nav div').eq(i).find('ul').innerWidth() > $('.nav').outerWidth()){
+//        $('.nav div').eq(i).find('li').css('float','right')
+//        $('.nav div').eq(i).find('ul').css('right',0)
+//    }
+//}
+//for(var i= 0,len = 0;i<$('.nav div').length;i++) {
+//    len += $('.nav div').eq(i).find('span').outerWidth();
+//    $('.nav div').eq(i+1).css('left',len);
+//    if(len + $('.nav div').eq(i+1).find('span').outerWidth() >$('.nav').innerWidth()){
+//        $('.nav div').eq(i+1).css('left', len - $('.nav').innerWidth()+8);
+//        $('.nav div').eq(i+1).css('top',30)
+//    }
+//}
